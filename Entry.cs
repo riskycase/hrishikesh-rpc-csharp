23b482d3cf1738d35f4721391b2844be251a41a9
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2021-09-08 18:03:40 +0530

    Initial commit
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

---
+++ Entry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Hrishikesh_s_RPC
+{
+    class Entry
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                LogonState logonState = new LogonState();
+                if (logonState.IsUnlocked())
+                {
+                    Console.WriteLine("Unlocked");
+                }
+                else
+                {
+                    Console.WriteLine("Locked");
+                }
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ LogonState.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace Hrishikesh_s_RPC
+{
+    internal class LogonState
+    {
+        public bool IsUnlocked()
+        {
+            Process[] processes = Process.GetProcessesByName("LogonUI");
+            if (processes.Length > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+
+}
