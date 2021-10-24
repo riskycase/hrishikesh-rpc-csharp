3a148a2c424ebfc22f43ba8c7f59dd0c93c7e863
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2021-10-24 15:40:24 +0530

    Add basic Discord activity code
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- Entry.cs
+++ Entry.cs
@@ -7,18 +7,61 @@ namespace Hrishikesh_s_RPC
     {
         static void Main(string[] args)
         {
-            while (true)
+            Discord.ActivityManager.UpdateActivityHandler updateActivityHandler = (result) =>
             {
-                LogonState logonState = new LogonState();
-                if (logonState.IsUnlocked())
+                if (result == Discord.Result.Ok)
                 {
-                    Console.WriteLine("Unlocked");
+                    Console.WriteLine("Success!");
                 }
                 else
                 {
-                    Console.WriteLine("Locked");
+                    Console.WriteLine("Failed");
                 }
-                Thread.Sleep(500);
+            };
+            bool unlocked = false;
+            var start = DateTime.Now;
+            var discord = new Discord.Discord(874718200997232710, (UInt64)Discord.CreateFlags.Default);
+            var discordActivityManager = discord.GetActivityManager();
+            var activity = new Discord.Activity {
+                Details = "Idling",
+                Timestamps =
+                {
+                    Start = ((DateTimeOffset)start).ToUnixTimeMilliseconds(),
+                },
+                Assets =
+                {
+                    LargeImage = "rog",
+                    LargeText = "test",
+                },
+                Instance = false
+            };
+            LogonState logonState = new LogonState();
+            while (true)
+            {
+                if (logonState.IsUnlocked())
+                {
+                    if (!unlocked)
+                    {
+                        start = DateTime.Now;
+                        unlocked = true;
+                        activity.Timestamps.Start = ((DateTimeOffset)start).ToUnixTimeMilliseconds();
+                        activity.Details = "Working";
+                        discordActivityManager.UpdateActivity(activity, updateActivityHandler);
+                    }
+                }
+                else
+                {
+                    if (unlocked)
+                    {
+                        start = DateTime.Now;
+                        unlocked = false;
+                        activity.Timestamps.Start = ((DateTimeOffset)start).ToUnixTimeMilliseconds();
+                        activity.Details = "Idling";
+                        discordActivityManager.UpdateActivity(activity, updateActivityHandler);
+                    }
+                }
+                discord.RunCallbacks();
+                Thread.Sleep(250);
             }
         }
     }
